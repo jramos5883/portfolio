ec129bacc872319ad038a6db9b47bbd8dd61e450
John Ramos <jramos5883@gmail.com>
2023-03-07 10:16:37 -0800

    CTA contact form is working and connected to my work email.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Comment
+function sendMail(params) {
+  const tempParams = {
+    from_name: document.getElementById("fromName").value,
+    to_name: document.getElementById("toName").value,
+    message: document.getElementById("msg").value,
+  };
+
+  emailjs
+    .send("service_06nu99m", "template_79kmp2a", tempParams)
+    .then(function (res) {
+      console.log("Success", res.status);
+    });
+}
