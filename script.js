2bc153974e54d0be154ab7949248cabf3f6b2786
John Ramos <jramos5883@gmail.com>
2023-03-08 14:31:30 -0800

    Finished rewriting my bio, ready to start applying everywhere, finger crossed..

--- script.js
+++ script.js
@@ -10,5 +10,10 @@ function sendMail(params) {
     .send("service_06nu99m", "template_79kmp2a", tempParams)
     .then(function (res) {
       console.log("Success", res.status);
+      alert('Message sent sucessfully.')
     });
+}
+
+function openResume() {
+  window.open('./JohnRamosTechResume.pdf', '_blank');
 }
